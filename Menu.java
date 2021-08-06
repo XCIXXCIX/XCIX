6c737fa4de4199a43f6745790c9ebbc00f955459
XCIXXCIX <bd_8_7_39@hotmail.com>
2021-08-06 13:24:06 +0700

    try pull

--- Menu.java
+++ Menu.java
@@ -1,6 +1,5 @@
 class Menu{
     public static void main(String[] args) {
         System.out.println("XCIX");
-        System.out.println("try");
     }
 }
--- Testcreate.java
+++ Testcreate.java
@@ -2,5 +2,6 @@ class Testcreate{
     public static void main(String[] args) {
         System.out.println("XCIX");
         System.out.println("try");
+        System.out.println("try pull");
     }
 }
